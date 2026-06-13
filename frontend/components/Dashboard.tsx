21a09125395ef1b6800bf07f4424ef6d453b4915
Saikat Bera <work.saikatbera@gmail.com>
2026-06-13 21:51:46 +0530

    district update

--- frontend/components/Dashboard.tsx
+++ frontend/components/Dashboard.tsx
@@ -395,7 +395,7 @@ function Dashboard({ addProduct, setCurrentPage, user }: DashboardProps) {
                   <option value="Burdwan">Burdwan District</option>
                   <option value="Shantiniketan">Shantiniketan District</option>
                   <option value="Birbhum">Birbhum District</option>
-                  <option value="Purulia">Birbhum District</option>
+                  <option value="Purulia">Purulia District</option>
                 </select>
               </div>
 
